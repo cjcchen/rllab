d9588005a42412c1eaaf5fd0bc196bbfcbdd6d94
cjcchen <ickchenjunchao@gmail.com>
2018-03-28 18:29:44 -0700

    use_dm control flatten function

--- rllab/envs/dm_control_env.py
+++ rllab/envs/dm_control_env.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from dm_control import suite
 from dm_control.rl.environment import StepType
+from dm_control.rl.control import flatten_observation
 
 from rllab.envs.base import Env, Step
 from rllab.envs.dm_control_view import DmControlViewer
@@ -15,47 +16,44 @@ to train and simulate your models.
 
 
 def _flat_shape(observation):
-    return np.sum( int(np.prod(v.shape)) for k,v in observation.items())
-
-
-def _flat_observation(observation):
-    return np.hstack(v.flatten() for k,v in observation.items())
+    return np.sum(int(np.prod(v.shape)) for k, v in observation.items())
 
 
 class DmControlEnv(Env, Serializable):
-    def __init__(self,
-                 domain_name,
-                 task_name,
-                 plot=False,
-                 width=320,
-                 height=240,
-                 ):
+    def __init__(
+            self,
+            domain_name,
+            task_name,
+            plot=False,
+            width=320,
+            height=240,
+    ):
         Serializable.quick_init(self, locals())
 
         self._env = suite.load(domain_name=domain_name, task_name=task_name)
 
         self._total_reward = 0
-        self._render_kwargs={ 'width': width, 'height': height }
+        self._render_kwargs = {'width': width, 'height': height}
 
         if plot:
             self._viewer = DmControlViewer()
         else:
             self._viewer = None
 
-
     def step(self, action):
         time_step = self._env.step(action)
         if time_step.reward:
             self._total_reward += time_step.reward
 
-        return Step(_flat_observation(time_step.observation), time_step.reward, \
+        return Step(flatten_observation(time_step.observation), \
+                time_step.reward, \
                 True if (time_step.step_type == StepType.LAST) else False, \
                 **time_step.observation)
 
     def reset(self):
         self._total_reward = 0
         time_step = self._env.reset()
-        return _flat_observation(time_step.observation)
+        return flatten_observation(time_step.observation)
 
     def render(self):
         if self._viewer:
@@ -79,4 +77,3 @@ class DmControlEnv(Env, Serializable):
     @property
     def total_reward(self):
         return self._total_reward
-
