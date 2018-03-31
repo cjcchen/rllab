b15289583ec0ccdc7c10c2adba629ad4365b1236
cjcchen <ickchenjunchao@gmail.com>
2018-03-30 19:05:39 -0700

    yapf format

--- scripts/run_experiment_lite.py
+++ scripts/run_experiment_lite.py
@@ -29,45 +29,95 @@ def run_experiment(argv):
 
     default_exp_name = 'experiment_%s_%s' % (timestamp, rand_id)
     parser = argparse.ArgumentParser()
-    parser.add_argument('--n_parallel', type=int, default=1,
-                        help='Number of parallel workers to perform rollouts. 0 => don\'t start any workers')
     parser.add_argument(
-        '--exp_name', type=str, default=default_exp_name, help='Name of the experiment.')
-    parser.add_argument('--log_dir', type=str, default=None,
-                        help='Path to save the log and iteration snapshot.')
-    parser.add_argument('--snapshot_mode', type=str, default='all',
-                        help='Mode to save the snapshot. Can be either "all" '
-                             '(all iterations will be saved), "last" (only '
-                             'the last iteration will be saved), "gap" (every'
-                             '`snapshot_gap` iterations are saved), or "none" '
-                             '(do not save snapshots)')
-    parser.add_argument('--snapshot_gap', type=int, default=1,
-                        help='Gap between snapshot iterations.')
-    parser.add_argument('--tabular_log_file', type=str, default='progress.csv',
-                        help='Name of the tabular log file (in csv).')
-    parser.add_argument('--text_log_file', type=str, default='debug.log',
-                        help='Name of the text log file (in pure text).')
-    parser.add_argument('--tensorboard_log_dir', type=str, default='progress',
-                        help='Name of the folder for tensorboard_summary.')
-    parser.add_argument('--tensorboard_step_key', type=str, default=None,
-                        help='Name of the step key in log data which shows the step in tensorboard_summary.')
-    parser.add_argument('--params_log_file', type=str, default='params.json',
-                        help='Name of the parameter log file (in json).')
-    parser.add_argument('--variant_log_file', type=str, default='variant.json',
-                        help='Name of the variant log file (in json).')
-    parser.add_argument('--resume_from', type=str, default=None,
-                        help='Name of the pickle file to resume experiment from.')
-    parser.add_argument('--plot', type=ast.literal_eval, default=False,
-                        help='Whether to plot the iteration results')
-    parser.add_argument('--log_tabular_only', type=ast.literal_eval, default=False,
-                        help='Whether to only print the tabular log information (in a horizontal format)')
-    parser.add_argument('--seed', type=int,
-                        help='Random seed for numpy')
-    parser.add_argument('--args_data', type=str,
-                        help='Pickled data for stub objects')
-    parser.add_argument('--variant_data', type=str,
-                        help='Pickled data for variant configuration')
-    parser.add_argument('--use_cloudpickle', type=ast.literal_eval, default=False)
+        '--n_parallel',
+        type=int,
+        default=1,
+        help=
+        'Number of parallel workers to perform rollouts. 0 => don\'t start any workers'
+    )
+    parser.add_argument(
+        '--exp_name',
+        type=str,
+        default=default_exp_name,
+        help='Name of the experiment.')
+    parser.add_argument(
+        '--log_dir',
+        type=str,
+        default=None,
+        help='Path to save the log and iteration snapshot.')
+    parser.add_argument(
+        '--snapshot_mode',
+        type=str,
+        default='all',
+        help='Mode to save the snapshot. Can be either "all" '
+        '(all iterations will be saved), "last" (only '
+        'the last iteration will be saved), "gap" (every'
+        '`snapshot_gap` iterations are saved), or "none" '
+        '(do not save snapshots)')
+    parser.add_argument(
+        '--snapshot_gap',
+        type=int,
+        default=1,
+        help='Gap between snapshot iterations.')
+    parser.add_argument(
+        '--tabular_log_file',
+        type=str,
+        default='progress.csv',
+        help='Name of the tabular log file (in csv).')
+    parser.add_argument(
+        '--text_log_file',
+        type=str,
+        default='debug.log',
+        help='Name of the text log file (in pure text).')
+    parser.add_argument(
+        '--tensorboard_log_dir',
+        type=str,
+        default='progress',
+        help='Name of the folder for tensorboard_summary.')
+    parser.add_argument(
+        '--tensorboard_step_key',
+        type=str,
+        default=None,
+        help=
+        'Name of the step key in log data which shows the step in tensorboard_summary.'
+    )
+    parser.add_argument(
+        '--params_log_file',
+        type=str,
+        default='params.json',
+        help='Name of the parameter log file (in json).')
+    parser.add_argument(
+        '--variant_log_file',
+        type=str,
+        default='variant.json',
+        help='Name of the variant log file (in json).')
+    parser.add_argument(
+        '--resume_from',
+        type=str,
+        default=None,
+        help='Name of the pickle file to resume experiment from.')
+    parser.add_argument(
+        '--plot',
+        type=ast.literal_eval,
+        default=False,
+        help='Whether to plot the iteration results')
+    parser.add_argument(
+        '--log_tabular_only',
+        type=ast.literal_eval,
+        default=False,
+        help=
+        'Whether to only print the tabular log information (in a horizontal format)'
+    )
+    parser.add_argument('--seed', type=int, help='Random seed for numpy')
+    parser.add_argument(
+        '--args_data', type=str, help='Pickled data for stub objects')
+    parser.add_argument(
+        '--variant_data',
+        type=str,
+        help='Pickled data for variant configuration')
+    parser.add_argument(
+        '--use_cloudpickle', type=ast.literal_eval, default=False)
 
     args = parser.parse_args(argv[1:])
 
