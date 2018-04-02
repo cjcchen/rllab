531c06c2bf19c505e8d4491da00876068465e13b
cjcchen <ickchenjunchao@gmail.com>
2018-04-02 16:31:04 -0700

    add histogram

---
+++ examples/example_tensorboard_logger.py
@@ -0,0 +1,10 @@
+import numpy as np
+from rllab.misc import logger 
+
+logger.set_tensorboard_dir("./test")
+
+for i in range(100):
+    val=np.random.normal(0,i,size=(3,3,3))
+    logger.record_tabular("app", i)
+    logger.record_histogram("gass", val)
+    logger.dump_tabular()
