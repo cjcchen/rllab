152187937ded63e4dc2e5b0f7f9f5cb3dbda5010
cjcchen <ickchenjunchao@gmail.com>
2018-05-02 02:48:09 +0800

    format

--- rllab/misc/tensorboard_summary.py
+++ rllab/misc/tensorboard_summary.py
@@ -15,19 +15,26 @@ class Summary:
         self._default_step = 0
         self._step_key = None
         self._writer = None
-        self._histogram_distribute_list = ['normal','gamma','poisson','uniform']
-        self._feed={}
-        
+        self._histogram_distribute_list = [
+            'normal', 'gamma', 'poisson', 'uniform'
+        ]
+        self._feed = {}
+
     def record_histogram(self, key, val):
         if str(key) not in self._histogram_ds:
             self._histogram_ds[str(key)] = tf.Variable(val)
             self._histogram_summary_op.append(
                 tf.summary.histogram(str(key), self._histogram_ds[str(key)]))
-            self._histogram_summary_op_merge = tf.summary.merge(self._histogram_summary_op)
+            self._histogram_summary_op_merge = tf.summary.merge(
+                self._histogram_summary_op)
 
         self._feed[self._histogram_ds[str(key)]] = val
 
-    def record_histogram_by_type(self,histogram_type, key = None, shape=[1000], **kwargs):
+    def record_histogram_by_type(self,
+                                 histogram_type,
+                                 key=None,
+                                 shape=[1000],
+                                 **kwargs):
         '''
         distribution type and args:
             normal: mean, stddev
@@ -36,17 +43,22 @@ class Summary:
             uniform: maxval
         '''
         if histogram_type not in self._histogram_distribute_list:
-            raise Exception('histogram type error %s' % histogram_type, 'builtin type',self._histogram_distribute_list)
+            raise Exception('histogram type error %s' % histogram_type,
+                            'builtin type', self._histogram_distribute_list)
 
         if str(key) not in self._histogram_ds:
-            self._histogram_ds[str(key)] = self.get_histogram_var_by_type(histogram_type, shape, **kwargs)
-            self._histogram_summary_op.append(tf.summary.histogram(str(key), self._histogram_ds[str(key)][0]))
-            self._histogram_summary_op_merge = tf.summary.merge(self._histogram_summary_op)
+            self._histogram_ds[str(key)] = self.get_histogram_var_by_type(
+                histogram_type, shape, **kwargs)
+            self._histogram_summary_op.append(
+                tf.summary.histogram(
+                    str(key), self._histogram_ds[str(key)][0]))
+            self._histogram_summary_op_merge = tf.summary.merge(
+                self._histogram_summary_op)
 
         key_list = self._histogram_ds[str(key)][1]
         val_list = self.get_histogram_val_by_type(histogram_type, **kwargs)
 
-        for key,val in zip(key_list, val_list):
+        for key, val in zip(key_list, val_list):
             self._feed[key] = val
 
     def record_scale(self, key, val):
@@ -68,7 +80,7 @@ class Summary:
                 self._writer.close()
                 self._writer = None
         else:
-            if dir_name[-1] !='/':
+            if dir_name[-1] != '/':
                 dir_name += '/'
             mkdir_p(os.path.dirname(dir_name))
             self._writer = tf.summary.FileWriter(dir_name)
@@ -88,12 +100,13 @@ class Summary:
         self._writer.flush()
         del self._summary_scale.value[:]
 
-    def get_histogram_var_by_type(self,histogram_type, shape, **kwargs):
+    def get_histogram_var_by_type(self, histogram_type, shape, **kwargs):
         if histogram_type == "normal":
             # Make a normal distribution, with a shifting mean
             mean = tf.Variable(kwargs['mean'])
             stddev = tf.Variable(kwargs['stddev'])
-            return tf.random_normal(shape=shape, mean=mean, stddev=stddev), [mean,stddev]
+            return tf.random_normal(
+                shape=shape, mean=mean, stddev=stddev), [mean, stddev]
         elif histogram_type == "gamma":
             # Add a gamma distribution
             alpha = tf.Variable(kwargs['alpha'])
@@ -106,9 +119,10 @@ class Summary:
             maxval = tf.Variable(kwargs['maxval'])
             return tf.random_uniform(shape=shape, maxval=maxval), [maxval]
 
-        raise Exception('histogram type error %s' % histogram_type, 'builtin type',self._histogram_distribute_list)
+        raise Exception('histogram type error %s' % histogram_type,
+                        'builtin type', self._histogram_distribute_list)
 
-    def get_histogram_val_by_type(self,histogram_type, **kwargs):
+    def get_histogram_val_by_type(self, histogram_type, **kwargs):
         if histogram_type == "normal":
             # Make a normal distribution, with a shifting mean
             return [kwargs['mean'], kwargs['stddev']]
@@ -122,5 +136,5 @@ class Summary:
             # And a uniform distribution
             return [kwargs['maxval']]
 
-        raise Exception('histogram type error %s' % histogram_type, 'builtin type',self._histogram_distribute_list)
-
+        raise Exception('histogram type error %s' % histogram_type,
+                        'builtin type', self._histogram_distribute_list)
