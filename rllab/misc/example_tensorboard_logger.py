2f6ae5588c07d3bd1bc2c738a2e3333e7812f4fd
cjcchen <ickchenjunchao@gmail.com>
2018-05-10 06:45:57 +0800

    add histogram

---
+++ rllab/misc/example_tensorboard_logger.py
@@ -0,0 +1,17 @@
+from rllab.misc import logger
+
+import numpy as np
+
+logger.set_tensorboard_dir("./histogram_example2")
+N = 400
+for i in range(N):
+    k_val = i / float(N)
+    logger.record_histogram_by_type('gamma', key='gamma', alpha=k_val)
+    logger.record_histogram_by_type(
+        'normal', key='normal', mean=5 * k_val, stddev=1.0)
+    logger.record_histogram_by_type('poisson', key='poisson', lam=k_val)
+    logger.record_histogram_by_type(
+        'uniform', key='uniform', maxval=k_val * 10)
+    logger.record_tabular("app", k_val)
+    logger.record_histogram("gass", k_val)
+    logger.dump_tensorboard(step=i)
